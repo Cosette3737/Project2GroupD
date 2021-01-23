b2657abe32ccac9cfa22591f0bc15f42e4f1d7e8
lisa427 <lisa.mcgowan4@gmail.com>
2021-01-23 10:21:00 -0500

    updated query

--- data/query.sql
+++ data/query.sql
@@ -1,8 +1,16 @@
-SELECT driverId, COUNT(place) AS "Number of Wins"
+CREATE VIEW wins AS
+SELECT driverId, COUNT(place) AS "wins"
 FROM (
 	SELECT driverId, place
 	FROM results 
 	WHERE place = '1'
 ) AS w
 GROUP BY driverId
-ORDER BY "Number of Wins" DESC;
+ORDER BY "wins" DESC;
+
+SELECT w.driverId, w.wins, d.forename, d.surname, d.nationality
+FROM wins AS w
+JOIN drivers AS d
+ON w.driverId=d.driverId
+ORDER BY w.wins DESC
+LIMIT 5;
