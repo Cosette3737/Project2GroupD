25879a2a3891d838b5c37ef9b7871342f8c90a83
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2021-01-26 23:30:59 -0600

    update barchart, css, viz1

--- static/js/script.js
+++ static/js/script.js
@@ -10,73 +10,74 @@ fetch('/top5_data')
 
  function createBar(topData) {
     console.log(topData)
-    var driverID = topData.map(drivers =>drivers[0]);
+    var wins = topData.map(drivers =>drivers[0]);
     var firstname = topData.map(drivers =>drivers[1]);
     var lastname = topData.map(drivers =>drivers[2]);
     var nationality = topData.map(drivers =>drivers[3]);
     var birthday = topData.map(drivers =>drivers[4]);
     var number = topData.map(drivers =>drivers[5]);
     var bio = topData.map(drivers =>drivers[6]);
-    var wins = [95, 91, 53, 51, 41];
+    //var wins = [95, 91, 53, 51, 41];
     var rank = [1,2,3,4,5]
     // var flags = ['http://upload.wikimedia.org/wikipedia/commons/a/ae/Flag_of_the_United_Kingdom.svg', 
     //             'http://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg',
     //             'http://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg',
     //             'http://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg',
     //             'http://upload.wikimedia.org/wikipedia/commons/O/05/Flag_of_Brazil.svg'];
-    console.log( nationality, birthday, number, bio);
-     var svgArea = d3.select("body").select("svg");
+    console.log( wins, rank,nationality, birthday, number, bio);
+    var svgArea = d3.select("body").select("svg");
      if (!svgArea.empty()) {
-         svgArea.remove();
-     }
+      svgArea.remove();
+  }
 
-    // svg params
-     var svgHeight = 600;
-     var svgWidth = 600;
+ // svg params
+    var svgHeight = 800;
+    var svgWidth = 100;
 
-     // margins
+  // margins
     var margin = {
-         top: 50,
-         right: 50,
-       bottom: 50,
-       left: 50
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
     };
 
-    // chart area minus margins
+ // chart area minus margins
     var chartHeight = svgHeight - margin.top - margin.bottom;
     var chartWidth = svgWidth - margin.left - margin.right;
 
-    // create svg container
+ // create svg container
     var svg = d3.select("body").append("svg")
         .attr("height", svgHeight)
-        .attr("width", svgWidth)
-    // shift everything over by the margins
+        .attr("width", svgWidth);
+ // shift everything over by the margins
     var chartGroup = svg.append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`)
-        .attr("rotate", 90);
-    // scale y to chart height
-        var yScale = d3.scaleLinear()
-        .domain([0, 100])
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        //.attr("rotate", 90);
+ // scale y to chart height
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(wins)])
         .range([chartHeight, 0]);
 
-    // scale x to chart width
-        var xScale = d3.scaleBand()
+ // scale x to chart width
+     var xScale = d3.scaleBand()
         .domain(lastname)
         .range([0, chartWidth])
         .padding(0.1);
 
-    // create axes
+ // create axes
     var yAxis = d3.axisLeft(yScale);
     var xAxis = d3.axisBottom(xScale);
 
-    // set x to the bottom of the chart
+ // set x to the bottom of the chart
     chartGroup.append("g")
         .attr("transform", `translate(0, ${chartHeight})`)
         .call(xAxis);
-
-    // set y to the y axis
+        
+ // set y to the y axis
     chartGroup.append("g")
         .call(yAxis);
+    console.log(yScale);
 
     chartGroup.selectAll("rect")
         .data(wins)
@@ -86,34 +87,39 @@ fetch('/top5_data')
         .attr("y", d => yScale(d))
         .attr("width", xScale.bandwidth())
         .attr("height", d => chartHeight - yScale(d))
-        .attr("fill", "green")
+        .attr("fill", "white")
         .on("click", clickHandler)
              // event listener for mouseover
-         .on("mouseover", function() {
+        .on("mouseover", function() {
            d3.select(this)
-                 .attr("fill", "red");
+                .attr("fill", "red")
+                .transition()
+                .duration(500)
          })
         // event listener for mouseout
         .on("mouseout", function() {
            d3.select(this)
-                 .attr("fill", "green");
+                 .attr("fill", "black")
+                 .transition()
+                .duration(500)
         });
-};
-//createBar();
- function clickHandler() {
-    console.log("this bitch works!");
-    var Textout = ( "Driver:" + {firstname} + {lastname});
-                    // "Rank: ${rank}" /n
-                    // "Wins: ${wins}" /n
-                    // "Nationality: ${nationality}" /n
-                    // "Birthday: ${birthday}" /n
-                    // "Driver Bio: <bio>" /n
-    ; 
-    console.log(Textout);
-    d3.select("#htmlEmbed").append("img")
-    .attr("src","http://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg")
-    //.attr(<i class="fa fa-trophy-alt"></i>)
-    .attr("width", 100)
-    .attr("height", 100)
-  }
 
+//createBar();
+    function clickHandler() {
+      console.log(firstname);
+        var Textout = ("Driver:");
+        //               ("Rank:" + (rank))
+        //              ("Wins:" + (wins))
+        //               ("Nationality:" + (nationality)) 
+        //              ("Birthday:" + (birthday))
+        //              ("Driver Bio:" + (bio)) 
+        var demo_meta = d3.select("#sample-metadata")
+        demo_meta.html("");
+        demo_meta.append("p").text(Textout);
+        console.log(demo_meta);
+        demo_meta.append("img")
+          .attr("src","http://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg")
+          .attr("width", 250)
+          .attr("height", 250)
+  }
+ };
